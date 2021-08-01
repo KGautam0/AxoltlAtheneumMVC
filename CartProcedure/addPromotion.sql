8a05d5d4c636587298d6be21ec3ee363d572bc56
EvanWard94 <42453890+EvanWard94@users.noreply.github.com>
2021-07-31 22:44:59 -0400

    Update addPromotion.sql

--- CartProcedure/addPromotion.sql
+++ CartProcedure/addPromotion.sql
@@ -1,4 +1,5 @@
-CREATE PROCEDURE addPromotion @PromoID INT, @PromoCode INT, @PromoStart DATE, @PromoEnd DATE, @PromoDiscount DOUBLE
-AS
-INSERT INTO promotions VALUES(@PromoID, @PromoCode, @PromoStart @PromoEnd, @PromoDiscount)
-GO;
+CREATE PROCEDURE addPromotion ( p_PromoID INT, p_PromoCode INT, p_PromoStart DATE, p_PromoEnd DATE, p_PromoDiscount DOUBLE)
+BEGIN
+INSERT INTO promotions(Promotion_ID, Promotion_Code, Promotion_Start, Promotion_End, Promotion_Discount) 
+VALUES(p_PromoID, p_PromoCode, p_PromoStart @PromoEnd, @PromoDiscount);
+END;
