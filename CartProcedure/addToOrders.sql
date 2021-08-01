6c5ec483a38ec76785bc4e49be6a865f079bc180
EvanWard94 <42453890+EvanWard94@users.noreply.github.com>
2021-07-31 22:45:24 -0400

    Update addToOrders.sql

--- CartProcedure/addToOrders.sql
+++ CartProcedure/addToOrders.sql
@@ -1,4 +1,5 @@
-CREATE PROCEDURE addToOrders @OrderID INT, @CustomerID VARCHAR(45), @OrderTime TIME, @OrderStatus VARCHAR(45), @OrderPrice INT, @OrderAdress VARCHAR(255), @OrderPayment VARCHAR(45), @OrderISBN VARCHAR(45)
-AS
-INSERT INTO orders VALUES(@OrderID, @CustomerID, @OrderTime, @OrderStatus, @OrderPrice, @OrderAdress, @OrderPayment, @OrderISBN)
-GO;
+CREATE PROCEDURE addToOrders ( p_OrderID INT, p_CustomerID VARCHAR(45), p_OrderTime TIME(6), p_OrderStatus VARCHAR(45), p_OrderPrice INT, p_OrderAdress VARCHAR(255), p_OrderPayment VARCHAR(45), p_OrderISBN VARCHAR(45))
+BEGIN
+INSERT INTO orders(Order_ID, Customer_ID, Order_Time, Order_Status, Order_Price, Order_Address, Order_Payment, Ordered_ISBN) 
+VALUES(p_OrderID, p_CustomerID, p_OrderTime, p_OrderStatus, p_OrderPrice, p_OrderAdress, p_OrderPayment, p_OrderISBN);
+END;
