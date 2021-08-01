f3484e39f1201fb9e8f143152c77d97e51c18581
DanielleE1998 <danielle.mawson.1998@gmail.com>
2021-08-01 18:13:45 -0400

    edit account - added three cards and promotional email option

--- DataAccessLayer/userDAL.cs
+++ DataAccessLayer/userDAL.cs
@@ -10,7 +10,7 @@ namespace AxolotlAtheneum.DataAccessLayer
 {
     public class userDAL
     {
-        private String connectionstring = "server=localhost;user id=root;database=bookstore;pwd=Twix!1721mnj";
+        private String connectionstring = "server=localhost;user id=root; Pwd=2020CoffeeXD!; database=axolotlatheneum";
 
         public bool insertUSER(User x)
         {
