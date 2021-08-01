7a846730b82bc54996f172252235e4152131c3a5
EvanWard94 <42453890+EvanWard94@users.noreply.github.com>
2021-07-31 22:44:43 -0400

    Update addBooktoCart.sql

--- CartProcedure/addBooktoCart.sql
+++ CartProcedure/addBooktoCart.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE addBookToCart @USERID INT, @ISBN INT, @PromoCode VARCHAR(45), @Quantity INT, @Price INT
-AS
-INSERT INTO shopping_carts VALUES(@USERID, @ISBN, @PromoCode, @Quantity * @Price)
-GO;
+CREATE PROCEDURE addBookToCart ( p_USERID INT, p_ISBN INT, p_PromoCode VARCHAR(45), p_Quantity INT, p_Price INT)
+BEGIN
+INSERT INTO shopping_carts(Customer_ID, Book_ISBN, Promo_Code, Cart_Total) VALUES(p_USERID, p_ISBN, p_PromoCode, p_Quantity * p_Price);
+END;
