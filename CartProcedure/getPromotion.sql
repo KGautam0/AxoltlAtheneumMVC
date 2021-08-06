386a7115c03d47150d0081357707ddece6d98c8d
EvanWard94 <42453890+EvanWard94@users.noreply.github.com>
2021-08-06 11:38:52 -0400

    Update getPromotion.sql

--- CartProcedure/getPromotion.sql
+++ CartProcedure/getPromotion.sql
@@ -1,4 +1,4 @@
 CREATE PROCEDURE getPromotion ( p_promotionCode VARCHAR(45))
 BEGIN
-SELECT * FROM promotions WHERE Promotion_Codee = p_promotionCode;
+SELECT * FROM promotions WHERE Promotion_Code = p_promotionCode;
 END;
